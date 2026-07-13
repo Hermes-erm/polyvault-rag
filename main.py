47cc1f9908ba559f4b2d5684d93f8268dbfcea2e
Hermes-erm <karan1raider@gmail.com>
2026-07-13 21:51:45 +0530

    mimetype

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from pathlib import Path
+from enum import Enum
 
 # from docling.document_extractor import DocumentExtractor
 from docling.document_converter import DocumentConverter, MarkdownFormatOption
@@ -40,13 +41,27 @@ converter = DocumentConverter(
 # with open(f"{doc_filename}.md", "w", encoding="utf-8") as fp:
 #     fp.write(conv_result.document.export_to_markdown())
 
+fileStagePath = Path("./pipeline/staging")
+
+
+class ContentType(Enum):
+    # JSON = "application/json"  # Optional
+    PDF = "application/pdf"
+    PNG = "image/png"
+    JPEG = "image/jpeg"
+    CSV = "text/csv"
+    XLSX = "application/vnd.ms-excel"
+
 
 @app.post("/file")
 async def import_file(
     file: UploadFile = File(...),
 ):  # File(...): expect multipart/form-data rather than JSON
 
-    with open(Path("./pipeline/staging") / f"{file.filename}", "wb") as fileDest:
+    if file.content_type not in ContentType:
+        raise HTTPException(415, "Unsupported file type")
+
+    with open(fileStagePath / f"{file.filename}", "wb") as fileDest:
         content = await file.read()
         fileDest.write(content)
 
