2fa69d22df2c8452cc42eb28ddd50d6fad9fd420
Hermes-erm <karan1raider@gmail.com>
2026-07-20 22:46:30 +0530

    add: LLMService

--- src/rag_system/generator.py
+++ src/rag_system/generator.py
@@ -1,3 +1,11 @@
+from google.genai import Client
+from dependencies import ai_client
+
+TEMPLATE = """
+"""
+
+
 class LLMService:
-    def __init__(self, llm_provider):
+    def __init__(self, llm_provider: Client, model: str = "gemini-3.5-flash"):
         self.llm_provider = llm_provider
+        self.model = model
