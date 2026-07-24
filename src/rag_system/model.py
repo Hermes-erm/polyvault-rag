8631d0c854e406dcd6ca26c202feff85b92cc175
Hermes-erm <karan1raider@gmail.com>
2026-07-24 20:36:23 +0530

    stashed change

--- src/rag_system/model.py
+++ src/rag_system/model.py
@@ -13,9 +13,12 @@ class Pipeline(Base):  # Table
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
     chunks = Column(Integer, default=0)
-    size = Column(Integer, default=0)
+    size = Column(String, default="0 B")
     status = Column(String)
     desc = Column(String, default="No description provided")
+
+    def __repr__(self):
+        return f"Pipeline(id={self.id}, name={self.name}, status={self.status}, chunks={self.chunks}, size={self.size}, desc={self.desc})"
 
 
 class PipelineRepository:
@@ -23,11 +26,10 @@ class PipelineRepository:
         pass
 
     def add_doc(self, db: Session, doc: PipelineSchema):
-        logger.debug(doc.model_dump_json(indent=2))
         doc_create = Pipeline(
             name=doc.filename,
             chunks=doc.chunks,
-            size=doc.size,
+            size=self._format_bytes(doc.size),
             status=doc.status,
             desc=doc.desc,
         )
@@ -36,10 +38,28 @@ class PipelineRepository:
         db.commit()
         db.refresh(doc_create)
 
-        logger.info("Data has been saved")
+        logger.debug("Data has been saved")
         logger.debug(doc_create)
 
         return doc_create
 
-    def update_doc_status(self):
-        pass
+    def update_doc(self, db: Session, doc: PipelineSchema, doc_id: int):
+        doc_result = db.query(Pipeline).filter(Pipeline.id == doc_id).first()
+
+        setattr(doc_result, "status", doc.status)
+        setattr(doc_result, "desc", doc.desc)
+        db.commit()
+        db.refresh(doc_result)
+
+        logger.debug("Data has been updated")
+        logger.debug(doc_result)
+
+        return doc_result
+
+    def _format_bytes(self, byte_size: int):
+        units = ["B", "KB", "MB", "GB"]
+
+        for unit in units:
+            if byte_size < 1024:
+                return f"{byte_size:.2f} {unit}"
+            byte_size = byte_size / 1024
